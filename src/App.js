dc91cca22f2546c00be4e89dee4cea33ad5f80a0
tom-s <thom.schell@gmail.com>
2018-12-13 00:25:21 +0100

    log

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
     this.editor.clear()
   }
   export = () => {
+    console.log("debug export", prevState.jixx)
     this.setState(prevState => ({
       jixxExport: prevState.jixx 
     }))
